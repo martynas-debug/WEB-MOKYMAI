b36429188457c6f7350f7225b881564cbb9acbb4
Martynas <mkraujaz@gmail.com>
2025-12-03 14:45:58 +0200

    committed

--- Javascript/9.3. salyga/9.3.js
+++ Javascript/9.3. salyga/9.3.js
@@ -5,6 +5,36 @@ console.log();
 
 let a = 78;
 
-if (a / 0) {
-    console.log('lyginis')
+if (a / 2) {
+    console.log('lyginis');
+} else {
+    console.log('atsakymas: 39');
 }
+
+// 2.
+console.log();
+console.log('------ 2 -----')
+console.log();
+
+let b = 44;
+
+if (a / 7) {
+    console.log('dalinasi');
+} else {
+    console.log('nesidalina');
+}
+
+// 3.
+console.log();
+console.log('------ 2 -----')
+console.log();
+
+let failas = 'failai/paskaitos/uzrasai.txt';
+
+if (failas.endsWith('.txt') ) {
+    console.log('tekstinis failas');
+} else {
+    console.log('kitokio formato tekstas')
+}
+
+console.log();
