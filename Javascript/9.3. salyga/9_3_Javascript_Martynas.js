78014827c04ba3432ab3a7d68b6d3dee2c340d56
Martynas <mkraujaz@gmail.com>
2025-12-04 09:22:29 +0200

    some update

---
+++ Javascript/9.3. salyga/9_3_Javascript_Martynas.js
@@ -0,0 +1,40 @@
+// 1.
+console.log();
+console.log('------ 1 -----')
+console.log();
+
+let a = 78;
+
+if (a / 2) {
+    console.log('lyginis');
+} else {
+    console.log('atsakymas: 39');
+}
+
+// 2.
+console.log();
+console.log('------ 2 -----')
+console.log();
+
+let b = 44;
+
+if (a / 7) {
+    console.log('dalinasi');
+} else {
+    console.log('nesidalina');
+}
+
+// 3.
+console.log();
+console.log('------ 2 -----')
+console.log();
+
+let failas = 'failai/paskaitos/uzrasai.txt';
+
+if (failas.endsWith('.txt') ) {
+    console.log('tekstinis failas');
+} else {
+    console.log('kitokio formato tekstas')
+}
+
+console.log();
