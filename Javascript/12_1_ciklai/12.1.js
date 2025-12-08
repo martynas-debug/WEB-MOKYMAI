21b6e23d1b49083652966a7ef093e25aea190ce7
Martynas <mkraujaz@gmail.com>
2025-12-08 11:47:50 +0200

    ciklai

---
+++ Javascript/12_1_ciklai/12.1.js
@@ -0,0 +1,30 @@
+// 1. 
+console.log('-----1-----'); 
+console.log();
+
+for (let i = 1; i <= 10; i++) {
+    console.log('skaicius:', i);
+}
+console.log();
+
+//2. 
+console.log('-----2-----'); 
+console.log();
+
+for (let i = 0; i <= 15; i++) {
+    if (i % 2 == 0) {
+    console.log('skaicius i = ' + i);
+    }
+}
+console.log();
+
+//3. 
+console.log('-----3-----'); 
+console.log();
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 == 0) {
+    console.log('skaicius i = ' + i);
+    }
+}
+console.log();
