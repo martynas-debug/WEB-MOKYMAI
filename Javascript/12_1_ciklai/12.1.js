fa74c8fdc62af1f1f312e478b600ab4cf62755b9
Martynas <mkraujaz@gmail.com>
2025-12-08 17:09:35 +0200

    dienos pabaiga

--- Javascript/12_1_ciklai/12.1.js
+++ Javascript/12_1_ciklai/12.1.js
@@ -11,10 +11,8 @@ console.log();
 console.log('-----2-----'); 
 console.log();
 
-for (let i = 0; i <= 15; i++) {
-    if (i % 2 == 0) {
+for (let i = 0; i <= 15; i+= 2) {
     console.log('skaicius i = ' + i);
-    }
 }
 console.log();
 
@@ -24,7 +22,61 @@ console.log();
 
 for (let i = 1; i <= 20; i++) {
     if (i % 3 == 0) {
-    console.log('skaicius i = ' + i);
+    console.log('[' + i + ']');
+    //console.log(`*${i}*`)
     }
 }
 console.log();
+
+//4. 
+console.log('-----4-----'); 
+console.log();
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 4 ==0){
+        console.log('skaicius', i, 'dalinasi is 4');
+    }
+}
+console.log();
+
+//5. 
+console.log('-----5-----'); 
+console.log();
+
+let pradzia = 5;
+let pabaiga = 10;
+
+if (pradzia < pabaiga){
+
+    for (let i = pradzia; i <= pabaiga; i++)
+     {
+        
+         console.log('skaicius', i, 'kvadratas', i * i);
+    }
+    
+    console.log();
+}
+
+
+
+
+//6. 
+console.log('-----6-----'); 
+console.log();
+
+let pradzia1 = 9, pradzia2 = 21;
+
+if(pradzia1 < pradzia2)
+{
+    for(let i = pradzia1; i <= pradzia2; i++){
+        
+    }
+   
+
+
+
+
+
+
+
+}
