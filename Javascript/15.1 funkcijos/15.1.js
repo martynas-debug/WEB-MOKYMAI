86fd66a3ac5572f6c9e59f75eb93a014a4f58cf0
Martynas <mkraujaz@gmail.com>
2025-12-16 09:15:08 +0200

    15.1 DONE

---
+++ Javascript/15.1 funkcijos/15.1.js
@@ -0,0 +1,86 @@
+// 1. 
+console.log('---1---');
+console.log();
+
+function vardasirkodel() {
+    console.log('Martynas');
+    console.log('Pasirinkau programavima nes noriu kad zmones dziaugtusi mano darbais ir jais naudotusi.');
+    console.log();
+}
+
+vardasirkodel();
+vardasirkodel();
+vardasirkodel();
+
+console.log();
+
+//2.
+console.log('---2---');
+console.log();
+
+function eilerastis() {
+    console.log('---');
+    console.log('lai rytas issaus');
+    console.log('lai saule pakils');
+    console.log('lai vakare nusileis');
+    console.log('ir ateis vel is naujo');
+    console.log('lai sukasi ratas sis');
+    console.log('---');
+}
+
+eilerastis();
+eilerastis();
+eilerastis();
+eilerastis();
+eilerastis();
+
+console.log('');
+
+// 3.
+
+console.log('---3---');
+console.log();
+
+function prisistatymas() {
+    console.log('labas, as toks ir anoks');
+    console.log();
+}
+
+function isejimas() {
+    console.log('na kagi, siandienai tiek, viso !');
+    console.log();
+}
+
+function grizimas() {
+    console.log('oj, pamirsau skrybele !');
+    console.log();
+}
+
+prisistatymas();
+isejimas();
+grizimas();
+
+console.log();
+
+// 4.
+console.log('---4---');
+console.log();
+
+function pirma() {
+    console.log('pati pradzia');
+    console.log();
+}
+
+function antra() {
+    console.log('pati pabaiga');
+    console.log();
+}
+
+function derinys() {
+    pirma();
+    antra();
+}
+
+derinys();
+
+console.log();
