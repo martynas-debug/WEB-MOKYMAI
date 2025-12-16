3c0468ea3a2c7053cef60a86b5dd7ba4b4c76b48
Martynas <mkraujaz@gmail.com>
2025-12-16 10:36:40 +0200

    15.2 DONE

---
+++ Javascript/15.2 funkcijos/15.2.js
@@ -0,0 +1,35 @@
+// 1.
+console.log('---1---');
+console.log();
+
+function pasisveikinimas(vard,) {
+    console.log('labas!', vard,);
+}
+
+function atsisveikinimas(vard) {
+    console.log('viso gero!', vard);
+}
+
+let vardas = 'Xenia'; 
+
+pasisveikinimas(vardas, 'Xenia');
+atsisveikinimas(vardas, 'Xenia'); 
+
+
+// 3.
+console.log('---3---');
+console.log();
+
+function autoduomenys(marke, modelis, gamybosmetai, darbinisturis) {
+    console.log('marke:', marke);
+    console.log('modelis:', modelis);
+    console.log('gamybosMetai:', gamybosmetai);
+    console.log('darbinisTuris:', darbinisturis);
+    console.log();
+}
+
+autoduomenys('AUDI', 'R8', 2008, 3.0);
+autoduomenys('BMW', 'I8', 2019, 3.3);
+
+console.log();
+
