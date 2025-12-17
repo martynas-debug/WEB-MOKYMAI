1adaaff709cf53a091e90c1b96bf1af79c26c9f0
Martynas <mkraujaz@gmail.com>
2025-12-17 09:23:01 +0200

    15.4 DONE

---
+++ Javascript/15.4 funkcijos/15.4.js
@@ -0,0 +1,33 @@
+// 1.
+
+console.log('---1---');
+console.log();
+
+function sakinys() {
+    let tekstas = 'labai grazus sakinys';
+    return tekstas;
+}
+
+console.log( sakinys());
+
+let atsakymas = sakinys();
+console.log(atsakymas);
+
+console.log();
+
+// 2.
+
+console.log('---2---');
+console.log();
+
+function skaicius() {
+    let skaicius = '34';
+    return skaicius;
+}
+
+console.log( skaicius () );
+
+let atsakymas1 = skaicius(); 
+console.log(atsakymas1);  
+
+console.log();
